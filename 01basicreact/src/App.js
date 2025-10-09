8177c949013edb2aed269d2e5f0b9e838eaaffba
aditya0814raj <adityaraj8559@gmail.com>
2025-10-09 06:18:29 +0530

    Add all project files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>CHAI aur REACT with CRA</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+  <h1>CHAI aur REACT with VITE</h1>
+  )
+}
+
+export default App
